a7e244872967c6f19489c3e5721d9d1a5cf1ba6d
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2019-07-20 10:08:42 +0430

    add laravel post-build commands

--- load_profile.py
+++ load_profile.py
@@ -1,17 +1,40 @@
 import os
+import subprocess
 
-cron = os.getenv('__CRON')
-cron_file = open('/etc/cron.d/liara_cron', 'w+')
+def setupCron():
+  cron = os.getenv('__CRON')
+  cron_file = open('/etc/cron.d/liara_cron', 'w+')
 
-if not cron:
+  if not cron:
+    cron_file.close()
+    return
+
+  envs = cron.split('$__SEP') if cron else []
+
+  for i in range(len(envs)):
+    interval = ' '.join(envs[i].split(' ')[:5])
+    command = ' '.join(envs[i].split(' ')[5:])
+    cron_file.write(interval + ' . /etc/load_envs.sh; ' + command + '\n')
+
   cron_file.close()
-  exit(0)
 
-envs = cron.split('$__SEP') if cron else []
+def setupPostBuildCommands():
+  postBuildCommands = os.getenv('__LARAVEL_POSTBUILDCOMMANDS')
 
-for i in range(len(envs)):
-  interval = ' '.join(envs[i].split(' ')[:5])
-  command = ' '.join(envs[i].split(' ')[5:])
-  cron_file.write(interval + ' . /etc/load_envs.sh; ' + command + '\n')
+  if not postBuildCommands:
+    return
 
-cron_file.close()
+  commands = postBuildCommands.split('$__SEP') if postBuildCommands else []
+
+  for i in range(len(commands)):
+    print('> post-build: ' + commands[i])
+    result = subprocess.run(commands[i].split(' '), stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+
+    print(result.stdout)
+
+    if result.returncode is not 0:
+      print('> post-build: command `' + commands[i] + '` returned a non-zore code: ' + str(result.returncode))
+      exit(1)
+
+setupCron()
+setupPostBuildCommands()
