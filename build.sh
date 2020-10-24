1e15cd13e4be2c3a4cd406f3259d06840e00b0c3
root <root@s5.liara>
2020-10-24 20:11:34 +0330

    WIP

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t liararepo/laravel-platform:frontend -f Dockerfile.frontend .
+#docker build -t liararepo/laravel-platform:frontend -f Dockerfile.frontend .
 
-docker build -t liararepo/laravel-platform:php7.2-backend -f Dockerfile.php7.2-backend .
-docker build -t liararepo/laravel-platform:php7.3-backend -f Dockerfile.php7.3-backend .
+#docker build -t liararepo/laravel-platform:php7.2-backend -f Dockerfile.php7.2-backend .
+#docker build -t liararepo/laravel-platform:php7.3-backend -f Dockerfile.php7.3-backend .
 docker build -t liararepo/laravel-platform:php7.4-backend -f Dockerfile.php7.4-backend .
