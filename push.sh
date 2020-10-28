67322b0808291a0a25a285dd0cabdbd48f409878
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-10-28 11:43:11 +0330

    Add push commands

--- push.sh
+++ push.sh
@@ -1,2 +1,5 @@
 docker push liararepo/laravel-platform:frontend
-docker push liararepo/laravel-platform:backend
+
+docker push liararepo/laravel-platform:php7.2-backend
+docker push liararepo/laravel-platform:php7.3-backend
+docker push liararepo/laravel-platform:php7.4-backend
